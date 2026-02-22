deb2f659798542b6ad765d8fb6aaaa2c8451d072
Gaurav Kumar <kumargaurav1073@gmial.com>
2026-02-22 17:35:51 +0530

    Home Page Created

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,49 +1,54 @@
 import { axiosInstance } from '@/lib/axios'
 import { useAuth } from '@clerk/clerk-react'
 import React, { useEffect, useState } from 'react'
-import { Loader, Loader2 } from 'lucide-react'
+import { Loader } from 'lucide-react'
+import { useAuthStore } from '@/stores/useAuthStore'
 
 const updateApiToken = (token: string | null) => {
-  if (token) {
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
-  }
-  else {
-    delete axiosInstance.defaults.headers.common['Authorization']
-  }
+    if (token) {
+        axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    }
+    else {
+        delete axiosInstance.defaults.headers.common['Authorization']
+    }
 }
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { getToken } = useAuth()
-  const [loading, setLoading] = useState(true)
+    const { getToken } = useAuth()
+    const [loading, setLoading] = useState(true);
+    const {checkAdminStatus} = useAuthStore()
 
-  useEffect(() => {
-    const initAuth = async () => {
-      try {
-        const token = await getToken()
-        updateApiToken(token)
-      }
-      catch (error) {
-        updateApiToken(null);
-        console.log("Error in auth provider", error);
+    useEffect(() => {
+        const initAuth = async () => {
+            try {
+                const token = await getToken()
+                updateApiToken(token)
+                if (token) {
+                    await checkAdminStatus();
+                }
+            }
+            catch (error) {
+                updateApiToken(null);
+                console.log("Error in auth provider", error);
 
-      } finally {
-        setLoading(false);
-      }
-    };
-    initAuth();
-  }, [getToken]);
+            } finally {
+                setLoading(false);
+            }
+        };
+        initAuth();
+    }, [getToken]);
 
-  if (loading) {
+    if (loading) {
+        return (
+            <div className='h-screen w-full flex items-center justify-center'>
+                <Loader className='size-8 text-emerald-500 animate-spin' />
+            </div>
+        )
+    }
+
     return (
-      <div className='h-screen w-full flex items-center justify-center'>
-        <Loader className='size-8 text-emerald-500 animate-spin' />
-      </div>
+        <>{children}</>
     )
-  }
-
-  return (
-    <>{children}</>
-  )
 }
 
 export default AuthProvider
