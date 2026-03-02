64a0d67702c70afe49eb5512678166cc215560cb
Gaurav Kumar <kumargaurav1073@gmial.com>
2026-03-02 15:41:27 +0530

    Chat Page Created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage"
-import { axiosInstance } from "./lib/axios"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
 import MainLayout from "./layout/MainLayout"
 import ChatPage from "./pages/chat/ChatPage"
 import AlbumPage from "./pages/album/AlbumPage"
 import AdminPage from "./pages/admin/AdminPage"
 import {Toaster} from "react-hot-toast"
+import NotFoundPage from "./pages/404/NotFoundPage"
 function App() {
-    const getSomeData = async () => {
-        await axiosInstance.get("/")
-    }
+    
     return (
         <>
             <Routes>
@@ -25,6 +23,7 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/chat" element={<ChatPage />} />
                     <Route path="/albums/:albumId" element={<AlbumPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
 
                 </Route>
             </Routes>
