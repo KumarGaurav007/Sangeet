cbbaecf824910a037e107537c7245d2723a7327b
Gaurav Kumar <kumargaurav1073@gmial.com>
2026-03-02 17:35:31 +0530

    deployment fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ initializeSocket(httpServer);
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: process.env.FRONTEND_URL,
 		credentials: true,
 	})
 );
@@ -72,7 +72,7 @@ app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
+	app.get("/", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 	});
 }
