43194f9850373ba72d6051da5fea096d549c6ce0
Gaurav Kumar <kumargaurav1073@gmial.com>
2026-03-03 10:12:59 +0530

    deployment bugs fixed

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.VITE_PUBLIC_API_URL,
 });
 
 axiosInstance.interceptors.request.use(async (config) => {
